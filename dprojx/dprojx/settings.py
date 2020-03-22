7e75cfd08f3ea15a88570e0deb1b9b1e2076e254
Dima <dimafedkk@mail.ru>
2020-03-22 13:59:07 +0300

    add url

--- dprojx/dprojx/settings.py
+++ dprojx/dprojx/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ija0d&121)tp_x*)_1kyswan(=wav@#xk-i=y4qd@*5%#v=n&c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.158.163.187']
 
 
 # Application definition
